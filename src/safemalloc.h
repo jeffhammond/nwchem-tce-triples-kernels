dced278c5cd316e426ff99f23ebfa8921166b14c
Jeff Hammond <jeff.science@gmail.com>
2014-02-26 13:22:46 -0600

    remove safemalloc source in favor of static; mess with buildsystem

--- src/safemalloc.h
+++ src/safemalloc.h
@@ -9,8 +9,22 @@
 
 int posix_memalign(void **memptr, size_t alignment, size_t size);
 
-#define ALIGNMENT 128
+#define ALIGNMENT 64
 
-void * safemalloc(size_t n);
+//void * safemalloc(size_t n);
+static void * safemalloc(size_t n)
+{
+    int rc = -1;
+    void * ptr = NULL;
+    rc = posix_memalign( &ptr, ALIGNMENT, n);
+
+    if ( ptr==NULL || rc!=0 )
+    {
+        fprintf( stderr , "%ld bytes could not be allocated \n" , (long)n );
+        exit(1);
+    }
+
+    return ptr;
+}
 
 #endif // SAFEMALLOC_H
