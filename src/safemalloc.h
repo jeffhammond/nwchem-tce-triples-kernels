05ed48d254071e0be5d27603c9265d28a3d7a379
jhammond <jhammond@login01-hf0.(none)>
2014-04-09 17:14:36 +0200

    blas tests soft-fail
    Signed-off-by: Jeff Hammond <jeff.science@gmail.com>

--- src/safemalloc.h
+++ src/safemalloc.h
@@ -15,13 +15,10 @@ static void * safemalloc(size_t n)
     int rc = -1;
     void * ptr = NULL;
     rc = posix_memalign( &ptr, ALIGNMENT, n);
-
-    if ( ptr==NULL || rc!=0 )
-    {
-        fprintf( stderr , "%ld bytes could not be allocated \n" , (long)n );
-        exit(1);
+    if ( ptr==NULL || rc!=0 ) {
+        fprintf( stderr , "%zu bytes could not be allocated \n" , n );
+	return NULL;
     }
-
     return ptr;
 }
 
