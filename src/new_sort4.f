01382adcc92a4f47de83c9882ec917bd68a3b3c8
Jeff Hammond <jeff.science@gmail.com>
2014-06-11 12:33:40 -0500

    full specialization on last index

--- src/new_sort4.f
+++ src/new_sort4.f
@@ -10,7 +10,23 @@
       jd(2) = b
       jd(3) = c
       jd(4) = d
-      if ((l.eq.4).or.(l.eq.3)) then
+      if (l.eq.4) then
+      do j1 = 1,a
+       id(1) = j1
+       do j2 = 1,b
+        id(2) = j2
+        do j3 = 1,c
+         id(3) = j3
+         do j4 = 1,d
+          id(4) = j4
+          ia = id(4)+jd(4)*(id(3)-1+jd(3)*(id(2)-1+jd(2)*(id(1)-1)))
+          ib = id(l)+jd(l)*(id(k)-1+jd(k)*(id(j)-1+jd(j)*(id(i)-1)))
+          s(ib) = u(ia) * factor
+         enddo
+        enddo
+       enddo
+      enddo
+      else if (l.eq.3) then
       do j1 = 1,a
        id(1) = j1
        do j2 = 1,b
