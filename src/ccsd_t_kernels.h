de393f030086a5da86f844714cae3791ee9425ab
Jeff Hammond <jehammond@nvidia.com>
2021-03-18 13:30:27 -0700

    remove blas from build

--- src/ccsd_t_kernels.h
+++ src/ccsd_t_kernels.h
@@ -5,5 +5,5 @@
 #include "ccsd_t_kernels_omp.h"
 #include "ccsd_t_kernels_1d.h"
 #include "ccsd_t_kernels_nd.h"
-#include "ccsd_t_kernels_blas.h"
 #include "ccsd_t_kernels_stdpar.h"
+//#include "ccsd_t_kernels_blas.h"
